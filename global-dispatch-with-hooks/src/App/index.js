7cc533576e9aad724b5c0f7c673bb22b495fb452
Christian Boyle <mail@christianboyle.com>
2021-08-27 09:10:58 -0500

    add global dispatch with hooks example

---
+++ global-dispatch-with-hooks/src/App/index.js
@@ -0,0 +1,185 @@
+import React, { useState, useReducer, useContext, createContext } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const DispatchContext = createContext(null);
+
+const initalTodos = [
+  {
+    id: uuidv4(),
+    task: 'Learn React',
+    complete: true,
+  },
+  {
+    id: uuidv4(),
+    task: 'Learn Firebase',
+    complete: true,
+  },
+  {
+    id: uuidv4(),
+    task: 'Learn GraphQL',
+    complete: false,
+  },
+];
+
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ALL':
+      return 'ALL';
+    case 'SHOW_COMPLETE':
+      return 'COMPLETE';
+    case 'SHOW_INCOMPLETE':
+      return 'INCOMPLETE';
+    default:
+      return state;
+  }
+};
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    case 'ADD_TODO':
+      return state.concat({
+        task: action.task,
+        id: uuidv4(),
+        complete: false,
+      });
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [filter, dispatchFilter] = useReducer(filterReducer, 'ALL');
+  const [todos, dispatchTodos] = useReducer(todoReducer, initalTodos);
+
+  // Global Dispatch Function
+  const dispatch = (action) =>
+    [dispatchTodos, dispatchFilter].forEach((fn) => fn(action));
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'ALL') {
+      return true;
+    }
+
+    if (filter === 'COMPLETE' && todo.complete) {
+      return true;
+    }
+
+    if (filter === 'INCOMPLETE' && !todo.complete) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <Filter />
+      <TodoList todos={filteredTodos} />
+      <AddTodo />
+    </DispatchContext.Provider>
+  );
+};
+
+const Filter = () => {
+  const dispatch = useContext(DispatchContext);
+
+  const handleShowAll = () => {
+    dispatch({ type: 'SHOW_ALL' });
+  };
+
+  const handleShowComplete = () => {
+    dispatch({ type: 'SHOW_COMPLETE' });
+  };
+
+  const handleShowIncomplete = () => {
+    dispatch({ type: 'SHOW_INCOMPLETE' });
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleShowAll}>
+        Show All
+      </button>
+      <button type="button" onClick={handleShowComplete}>
+        Show Complete
+      </button>
+      <button type="button" onClick={handleShowIncomplete}>
+        Show Incomplete
+      </button>
+    </div>
+  );
+};
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ))}
+  </ul>
+);
+
+const TodoItem = ({ todo }) => {
+  const dispatch = useContext(DispatchContext);
+
+  const handleChange = () =>
+    dispatch({
+      type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+      id: todo.id,
+    });
+
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={handleChange}
+        />
+        {todo.task}
+      </label>
+    </li>
+  );
+};
+
+const AddTodo = () => {
+  const dispatch = useContext(DispatchContext);
+
+  const [task, setTask] = useState('');
+
+  const handleSubmit = (event) => {
+    if (task) {
+      dispatch({ type: 'ADD_TODO', task });
+    }
+
+    setTask('');
+
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => setTask(event.target.value);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={task} onChange={handleChange} />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default App;
